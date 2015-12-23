76b448be144966e5e078a4e3b0ec09f97e63ed90
Dklenye <dmtKlenye@gmail.com>
2015-12-23 12:00:15 +0300

    [add] Добавлены модульные тесты

---
+++ SqlChangeDataLog.Tests/ExtensionsTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using SqlChangeDataLog.Extensions;
+
+namespace SqlChangeDataLog.Tests
+{
+    public class ExtensionsTests
+    {
+        [Test]
+        public void StringApplyTemplateTest()
+        {
+            var tpl = "I'm travelling {one} the {two}";
+
+            var o1 = new { one = "down", two = 12 };
+            var o2 = new { one = 33, two = "wood" };
+
+            Assert.AreEqual("I'm travelling down the 12", tpl.ApplyTemplate(o1));
+            Assert.AreEqual("I'm travelling 33 the wood", tpl.ApplyTemplate(o2));
+        }
+    }
+}
---
+++ SqlChangeDataLog.Tests/TriggerTextBuilderTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using NUnit.Framework;
+using SqlChangeDataLog.Triggers;
+
+namespace SqlChangeDataLog.Tests
+{
+    public class TriggerTextBuilderTests
+    {
+        protected Trigger CreateTrigger(string operation)
+        {
+            return new Trigger()
+            {
+                Columns = new[] { "Id", "Name" },
+                Operation = operation,
+                LogTableName = "ChangeLog",
+                TableName = "TestTable",
+                ExtendedLogic = @"IF (@@RowCount > 1)
+RETURN"
+            };
+        }
+
+        protected string BuildText(string operation)
+        {
+            return new TriggerTextBuilder(CreateTrigger(operation), "Id","Some_ChangeLog").BuildTriggerText();
+        }
+
+        protected bool ExistsBrackets(string text)
+        {
+            return new Regex("[{}]").IsMatch(text);
+        }
+
+        [Test]
+        public void Check_SqlTemplate_Complete_For_Insert()
+        {
+            var text = BuildText("INSERT");
+            Console.Write(text);
+            Assert.True(!ExistsBrackets(text));
+        }
+        [Test]
+        public void Check_SqlTemplate_Complete_For_Update()
+        {
+            var text = BuildText("update");
+            Console.Write(text);
+            Assert.True(!ExistsBrackets(text));
+
+        }
+        [Test]
+        public void Check_SqlTemplate_Complete_For_Delete()
+        {
+            var text = BuildText("DeLeTe");
+            Console.Write(text);
+            Assert.True(!ExistsBrackets(text));
+        }
+
+    }
+}
