8a1a91b4749c893d90d9440fe8d95535d81d7701
Dklenye <dmtKlenye@gmail.com>
2016-01-14 09:37:02 +0300

    [upd] Модифицированы регулярные выражения для нового шаблона триггера

--- SqlChangeDataLog/Triggers/TriggerTextParser.cs
+++ SqlChangeDataLog/Triggers/TriggerTextParser.cs
@@ -6,13 +6,10 @@ namespace SqlChangeDataLog.Triggers
     public class TriggerTextParser
     {
         private const string OperationPattern = @"(?<=for\s+)insert|update|delete";
-        private const string LegacySelectXmlPattern = @"(?<=set\s+@xml\s+=\s+[(]).*?FOR XML AUTO";
-        private const string SelectXmlPattern = @"(?<=[(])\s*SELECT.*FOR XML AUTO";
-        private const string SelectColumnsPattern = @"(?<=select).*?(?=from\s?[(])";
+        private const string SelectXmlPattern = @"(?<=[(]\s*)SELECT.*FOR XML AUTO";
+        private const string SelectColumnsPattern = @"^.*?(?=\s*\n?\s*from)";
         private const string RemoveSelectPattern = @"\s*select\s+(top\s+\d+\s+)?";
-        private const string RemoveSpacesPattern = @"s*\r+\s*";
         private const string RemoveBracketsPattern = @"[\[\]]";
-
 
         public TriggerTextParser(string text)
         {
@@ -27,11 +24,11 @@ namespace SqlChangeDataLog.Triggers
             var regex1 = CreateRegex(SelectXmlPattern);
             var selectXml = regex1.Match(Text).ToString();
 
-            var regex2 = CreateRegex(RemoveSpacesPattern);
-            var columnsWithNoSpaces = regex2.Replace(selectXml, "");
-
+            var regex2 = CreateRegex(SelectColumnsPattern);
+            var selectColumns = regex2.Match(selectXml).ToString();
+            
             var regex3 = CreateRegex(RemoveSelectPattern);
-            var columnsWithBrackets = regex3.Replace(columnsWithNoSpaces, "");
+            var columnsWithBrackets = regex3.Replace(selectColumns, "");
 
             var regex4 = CreateRegex(RemoveBracketsPattern);
             var columns = regex4.Replace(columnsWithBrackets, "");
